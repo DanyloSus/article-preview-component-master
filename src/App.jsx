8441ef0caadfc0fd5b28ea56167d8dd091bb8bf8
Danylo Sus <danil.sushko19@gmail.com>
2023-07-25 15:55:53 +0300

    Desktop design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Content from "./Elements/Content";
 
 function App() {
-  return <h1>Sus</h1>;
+  return (
+    <main className="w-[50%] bg-white h-[33.3333%] flex overflow-x-visible rounded-xl">
+      <img
+        src="./drawers.jpg"
+        alt="drawers"
+        className="w-[42%] object-cover object-left rounded-l-xl"
+      />
+      <Content />
+    </main>
+  );
 }
 
 export default App;
